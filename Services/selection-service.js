fbfff0a0c240b679543bfccd38fe1aaf560547a8
jajaspider <jaja_spider@naver.com>
2021-06-13 12:53:39 +0900

    init project

---
+++ Services/selection-service.js
@@ -0,0 +1,10 @@
+const botConfig = require("../config");
+
+function run(command,message){
+    console.log(command);
+    console.log(message);
+
+
+}
+
+module.exports = {run};
---
+++ config.js
@@ -0,0 +1,23 @@
+
+
+const MAPLESTORY_COMMAND = {
+    // 뭐먹지 뭐하지 채널
+    RANDOM_SELECTION : {
+        NAME : "RANDOM_SELECTION",
+        EAT : ["뭐먹지","ㅁㅁㅈ","aaw"],
+        DOING : ["뭐하지","ㅁㅎㅈ","agw"],
+        CHANNEL : ["!채널","!ㅊㄴ"]
+    },
+
+    KAKAO_LINK : {
+        NAME : "KAKAO_LINK",
+        INFO : ["!정보","!ㅈㅂ"],
+        EVENT : ["!이벤트","!ㅇㅂㅌ"]
+    }
+};
+
+const LOSTARK_COMMAND = {
+
+};
+
+module.exports = {MAPLESTORY_COMMAND};
