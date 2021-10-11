917d9f78a881e67250b76e4b0e920afba21b9a0e
jajaspider <jaja_spider@naver.com>
2021-10-11 21:22:50 +0900

    fix(commandManager)
    commandManager support for some commands

--- config/constants.js
+++ config/constants.js
@@ -1,9 +1,13 @@
+/**
+ * description : 기본 명령어
+ */
 const COMMAND_SELECTION = {
-  methods: [{
+  methods: [
+    {
       name: "!뭐먹지",
       alias: ["!ㅁㅁㅈ", "!aaw"],
       params: {
-        type: "food"
+        type: "food",
       },
       description: "도움말에쓸말",
     },
@@ -11,7 +15,15 @@ const COMMAND_SELECTION = {
       name: "!뭐하지",
       alias: ["!ㅁㅎㅈ", "!agw"],
       params: {
-        type: "doing"
+        type: "doing",
+      },
+      description: "도움말에쓸말",
+    },
+    {
+      name: "!채널",
+      alias: ["ㅊㄴ"],
+      params: {
+        type: "channel",
       },
       description: "도움말에쓸말",
     },
@@ -19,24 +31,96 @@ const COMMAND_SELECTION = {
 };
 
 const COMMAND_HELP = {
-  methods: [{
-    name: "!도움말",
-    alias: [""],
-    params: {},
-    description: "도움말을 보여줍니다."
-  }]
-}
+  methods: [
+    {
+      name: "!도움말",
+      alias: [""],
+      params: {},
+      description: "도움말을 보여줍니다.",
+    },
+  ],
+};
 
+/**
+ * description : 메이플스토리 명령어
+ */
+const COMMAND_MUTO = {
+  methods: [
+    {
+      name: "!무토",
+      alias: ["!ㅁㅌ"],
+      description: "도움말에쓸말",
+    },
+  ],
+};
 
-const MAPLESTORY_COMMAND = [];
-const LOSTARK_COMMAND = [];
+const COMMAND_BOSS = {
+  methods: [
+    {
+      name: "!보스",
+      alias: ["!ㅄ", "!ㅂㅅ"],
+      description: "도움말에쓸말",
+    },
+  ],
+};
+
+const COMMAND_MESO = {
+  methods: [
+    {
+      name: "!메소",
+      alias: ["!ㅁㅅ"],
+      description: "도움말에쓸말",
+    },
+  ],
+};
+
+/**
+ * description : 로스트아크 명령어
+ */
+
+const COMMAND_EMOTICONLIST = {
+  methods: [
+    {
+      name: "!이모티콘리스트",
+      alias: ["!ㅇㅁㅌㅋㄽㅌ", "!ㅇㅁㅌㅋㄹㅅㅌ"],
+      description: "도움말에쓸말",
+    },
+  ],
+};
+
+/**
+ * description : 관리자 명령어
+ */
+
+const COMMAND_REGISTER = {
+  methods: [
+    {
+      name: "!등록",
+      alias: [],
+    },
+  ],
+};
+
+const MAPLESTORY_COMMAND = {
+  MUTO: COMMAND_MUTO,
+  BOSS: COMMAND_BOSS,
+  MESO: COMMAND_MESO,
+};
+const LOSTARK_COMMAND = {
+  EMOTICONLIST: COMMAND_EMOTICONLIST,
+};
 const COMMON_COMMAND = {
   HELP: COMMAND_HELP,
-  SELECTION: COMMAND_SELECTION
+  SELECTION: COMMAND_SELECTION,
+};
+
+const MANAGER_COMMAND = {
+  REGISTER: COMMAND_REGISTER,
 };
 
 module.exports = {
   MAPLESTORY_COMMAND,
   LOSTARK_COMMAND,
   COMMON_COMMAND,
+  MANAGER_COMMAND,
 };
--- index_copy.js
+++
@@ -1,9 +0,0 @@
-const { commandValidator } = require("./commandManager");
-let message = "agw 1";
-let messageSplit = message.split(" ");
-
-let serviceName = commandValidator(messageSplit[0]);
-
-if (serviceName) {
-  console.log(serviceName);
-}
